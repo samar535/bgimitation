9448808293964074911be3c91b4165470814f76e
Shubham Soni <159618449+shubham-kth@users.noreply.github.com>
2025-12-20 17:02:00 +0530

    update body font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} ${playfair.className}`}>
+      <body className={`${inter.className}`}> 
+      {/* <body className={`${inter.className} ${playfair.className}`}>  */}
         <Toaster position="top-right" />
         {children}
       </body>
